54301d1786377fb0327fa8a3ec1fcf2914b9bdf6
Raymond Nguyen <125716839+Fenaum@users.noreply.github.com>
2024-09-09 17:59:43 -0700

    added bracket logics and tested feature: moving round up after each match

--- src/ts/brackets.ts
+++ src/ts/brackets.ts
@@ -1,19 +1,55 @@
-let testPlayers = ['p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7', 'p8']
-let testBrackets: any = [];
+import { createGame } from "./gameLogic";
 
-function initializeBracket(...players: any) {
-    // 
-    if (players.length % 4 === 0) {
+export function createBrackets(...players: any) {
+  return {
+    brackets: {} as { [key: string]: any[] },
+    roundth: 1 as number, // start with round 1
+    initializeBrackets: function () {
+      if (players.length % 4 === 0) {
+        this.brackets[`round${this.roundth}`] = [];
+
         for (let i = 0; i < players.length; i += 2) {
-            let player1 = players[i]
-            let player2 = players[i+1]
-            console.log (player1, player2)
-            testBrackets.push({player1: player1, player2: player2})
-            console.log(testBrackets)
+          let player1 = players[i];
+          let player2 = players[i + 1];
+          this.brackets[`round${this.roundth}`].push({
+            player1,
+            player2,
+            winner: "",
+          });
         }
-    } else {
-        console.log('Not enough players')
-    }
+      } else {
+        console.log("Not enough players");
+      }
+    },
+    startTournament: function () {
+        
+      // Your logic for starting the tournament
+      for (let round in this.brackets) {
+        // console.log(this.brackets[round]);
+        for (let pair of this.brackets[round]) {
+          // const game = createGame(pair[0],pair[1])
+          // Access pair properties directly instead of using player1 and player2
+          let game = createGame(pair.player1, pair.player2);
+          game.playGame();
+          pair.winner = game.gameState.winner;
+          console.log(this.brackets)
+        }
+      }
+    },
+  };
 }
 
-initializeBracket(...testPlayers)
+let testPlayers = [
+  { name: "john" },
+  { name: "Vayne" },
+  { name: "Jane" },
+  { name: "Michael" },
+  { name: "Sarah" },
+  { name: "William" },
+  { name: "Emily" },
+  { name: "David" },
+];
+
+const testbrackets = createBrackets(...testPlayers);
+testbrackets.initializeBrackets();
+testbrackets.startTournament();
